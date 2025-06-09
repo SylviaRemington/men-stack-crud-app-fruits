ada5c25364d1a07747141cea8acbea735a653b28
SylviaRemington <sylviaremington@gmail.com>
2025-06-09 04:51:05 -0700

    adding key value pairs to be required

--- models/fruit.js
+++ models/fruit.js
@@ -2,8 +2,14 @@
 
 const mongoose = require('mongoose');
 
+// const fruitSchema = new mongoose.Schema({
+//   name: String, //this defines what key values it should have
+  //also changing these to be required so not dodgy data
+//   isReadyToEat: Boolean,
+// });
+
 const fruitSchema = new mongoose.Schema({
-  name: String, //this defines what key values it should have
+  name: {type: String, required: true, unique: true}, //so you can only have one banana and one apple
   isReadyToEat: Boolean,
 });
 
