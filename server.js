95131bdbab9f9e1289df4ed44831581976d78f03
SylviaRemington <sylviaremington@gmail.com>
2025-06-09 04:00:39 -0700

    adding dotenv installing it & adding dotenv server.js code

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Here is where we import modules
 // We begin by loading Express
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 
 const app = express();
