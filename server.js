78d7704fff72c680da39fbc632b693f4e6e00cb1
SylviaRemington <sylviaremington@gmail.com>
2025-06-09 04:28:03 -0700

    adding string template for listening on port for better practice for cloud computing

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ mongoose.connection.on("connected", () => {
 // });
 
 app.listen(process.env.PORT, () => {
-  console.log('Listening on port 3000');
+  console.log(`Listening on port ${process.env.PORT}`);
 });
