093315fa59b12bf5fbbd77dd32db9e35b26ab1c7
SylviaRemington <sylviaremington@gmail.com>
2025-06-09 04:16:17 -0700

    adding mongoose connect to connect to MongoDB database

--- server.js
+++ server.js
@@ -3,13 +3,23 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //requiring the package
 
 const app = express();
 
 app.get("/", async (req, res) => { // have an end point set up here
   res.render("index.ejs"); //changing res.send to res.render
 });
+
+// Connect to MongoDB using the connection string in the .env file
+// mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
