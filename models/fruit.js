884bb1fecbad6cb216a14eaaf1db4ddaffb12e9f
SylviaRemington <sylviaremington@gmail.com>
2025-06-09 04:54:41 -0700

    notes for required key value pairs

--- models/fruit.js
+++ models/fruit.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 
 const fruitSchema = new mongoose.Schema({
   name: {type: String, required: true, unique: true}, //so you can only have one banana and one apple
-  isReadyToEat: Boolean,
+  isReadyToEat: {type: Boolean, default: false}, //it's not required but if you didn't provide it, it would put it in anyway as false
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema); // This is what creates the model.
